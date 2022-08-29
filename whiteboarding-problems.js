de66917b040eace35a61de55c25680cac9f82a27
Samuel Park <s.park111497@gmail.com>
2022-08-29 10:27:09 -0700

    Add files via upload

---
+++ whiteboarding-problems.js
@@ -0,0 +1,27 @@
+/************************* Whiteboarding Practice *************************
+ * Use your knowledge of data structures and time complexity to solve the following problems. 
+ * These type of problems will not show up on the assessment but may be tested in coding interviews.
+ * 
+ * NOTE - Be sure to apply Polya's Problem Solving framework when solving these problems:
+ * - Understand the problem
+ * - Devise a plan
+ * - Carry out the plan
+ * - Look back and refactor if necessary
+ * 
+ * NOTE - In the context of this lesson, try to follow some of these steps when evaluating your solutions:
+ * - Determine and explain the time and space complexity of a function after you come to a solution
+ * - Discuss the potential for a more optimal solution
+ * - If a more optimal solution is discussed, implement that solution 
+ ******************************************************************************/
+
+// 1) logBetween
+
+function logBetwen(lowNum, highNum) {
+
+}
+
+// 2) logBetweenStepper
+
+// 3) printReverse
+
+// 4) fizzBuzz
